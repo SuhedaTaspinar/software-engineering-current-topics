caf4c425c46d0f23750719bc650c233d57454419
sdtaspinar <suhedadilektaspinar@gmail.com>
2024-12-06 18:24:07 +0300

    fixed

--- app.js
+++ app.js
@@ -7,16 +7,17 @@ const path = require("path");
 
 const app = express();
 
+// Statik Dosyalar
 app.use(express.static(path.join(__dirname, 'public')));
 
 // MongoDB Bağlantısı
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("MongoDB connected"))
-    .catch(err => console.log(err));
+    .catch(err => console.error("MongoDB connection error:", err));
 
 // Middleware
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
 app.use(session({ secret: "secret", resave: false, saveUninitialized: true }));
 app.use(flash());
 
@@ -29,10 +30,10 @@ app.use((req, res, next) => {
 
 // View Engine
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 // Rotalar
 app.use("/", require("./routes/index"));
 
-// Server Başlat
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// Vercel için uygulamayı dışa aktar
+module.exports = app;
